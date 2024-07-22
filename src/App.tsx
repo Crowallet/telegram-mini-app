09c23bad774cb95c453f37d85a82d03644c0c097
Ilya Mikheev <ilyamikheev@MacBook-Pro-Ilya.local>
2024-07-22 23:00:25 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import twaLogo from './assets/tapps.png'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import WebApp from '@twa-dev/sdk'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Crowallet</h1>
+    </>
+  )
+}
+
+export default App
